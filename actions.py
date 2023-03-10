d386701317629355189ef6e8bf3050952f0016f2
AdityaParanjape <62301803+AdityaParanjape@users.noreply.github.com>
2023-03-10 21:24:03 +0530

    Add files via upload

--- actions.py
+++ actions.py
@@ -15,7 +15,6 @@ from rasa_sdk.events import SlotSet
 from rasa_sdk.events import FollowupAction
 
 #
-#
 # class ActionHelloWorld(Action):
 #
 #     def name(self) -> Text:
@@ -29,51 +28,6 @@ from rasa_sdk.events import FollowupAction
 #
 #         return []
 
-# class ActionPlaySong(Action):
-
-#     def name(self) -> Text:
-
-#         return "action_play_song"
-
-#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-#         dispatcher.utter_message(text = "I will now send GET request and call the spotify API")
-
-#         return []
-
-class ActionSetAPISlot_Music(Action):
-
-    def name(self) -> Text:
-        
-        return "action_set_api_slot_music"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-        print("reached set api slot func")
-        return[SlotSet("API_num", "one")]
-
-class ActionSetAPISlot_Location(Action):
-
-    def name(self) -> Text:
-        
-        return "action_set_api_slot_location"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-        print("reached set api location slot func")
-        return[SlotSet("API_num", "two")]
-
-class ActionSetAPISlot_Contacts(Action):
-
-    def name(self) -> Text:
-        
-        return "action_set_api_slot_contact"
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-        print("reached set api contact slot func")
-        return[SlotSet("API_num", "three")]
-
 class ActionCheckSong(Action):
 
     def name(self) -> Text:
@@ -82,6 +36,7 @@ class ActionCheckSong(Action):
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
+        print("reached check song method")
         user_choice = tracker.get_slot('song_name')
         dispatcher.utter_message(text = f"You chose the song {user_choice}")
 
@@ -94,7 +49,8 @@ class ActionCheckLocation(Action):
         return "action_check_location"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
+        
+        print("reached check location method")
         user_choice = tracker.get_slot('location_name')
         dispatcher.utter_message(text = f"You chose the location {user_choice}")
 
@@ -108,8 +64,9 @@ class ActionCheckContact(Action):
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
+        print("reached check contact method")
         user_choice = tracker.get_slot('contact_name')
-        dispatcher.utter_message(text = f"You chose the song {user_choice}")
+        dispatcher.utter_message(text = f"You chose the person {user_choice}")
 
         return []
 
@@ -133,7 +90,7 @@ class ConfirmSong(Action):
 
         if user_conf in resp_list :
             print("reached")
-            return [FollowupAction("action_set_api_slot_music")]
+            return [FollowupAction("action_call_spotify_api")]
 
 class ConfirmLocation(Action):
 
@@ -155,7 +112,7 @@ class ConfirmLocation(Action):
 
         if user_conf in resp_list :
             print("reached")
-            return [FollowupAction("action_set_api_slot_location")]
+            return [FollowupAction("action_call_maps_api")]
 
 class ConfirmContact(Action):
 
@@ -177,123 +134,56 @@ class ConfirmContact(Action):
 
         if user_conf in resp_list :
             print("reached")
-            return [FollowupAction("action_set_api_slot_contact")]
+            return [FollowupAction("action_call_contacts_api")]
 
-class ActionCallAPI(Action):
+
+class ActionCallSpotifyAPI(Action):
 
     def name(self) -> Text:
 
-        return "action_call_api"
+        return "action_call_spotify_api"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
-        print("reached in api call func")
-        if(tracker.get_slot('API_num') == 'one'):
-            dispatcher.utter_message(text = "I will now send GET request and call the SpotifyAPI")
-        elif(tracker.get_slot('API_num') == 'two'):
-            dispatcher.utter_message(text = "I will now send GET request and call the GoogleMaps API")
-        elif(tracker.get_slot('API_num') == 'three'):
-            dispatcher.utter_message(text = "I will now send GET request and call the Contacts API")
-        else:
-            dispatcher.utter_message(text = "No value was set in the API num slot")
+        user_choice = tracker.get_slot("song_name")
+        print(f"Now calling the Spotify API to play the song {user_choice}")
+        dispatcher.utter_message(f"I will now call the Spotify API to play the song {user_choice}")
 
         return []
+
+
+class ActionCallMapsAPI(Action):
+
+    def name(self) -> Text:
+
+        return "action_call_maps_api"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
+        user_choice = tracker.get_slot("location_name")
+        print(f"Now calling the Maps API to play the song {user_choice}")
+        dispatcher.utter_message(f"Now calling the Maps API to find the location {user_choice}")
 
-# class ActionOpenMaps(Action):
+        return []
 
-#     def name(self) -> Text:
 
-#         return "action_open_maps"
+class ActionCallContactsAPI(Action):
 
-#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def name(self) -> Text:
 
-#         dispatcher.utter_message(text = "I will now send GET request and call the GoogleMaps API")
+        return "action_call_contacts_api"
 
-#         return []
-
-# class ActionSetAPISlot_Maps(Action):
-
-#     def name(self) -> Text:
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
-#         return "action_set_api_slot_maps"
+        user_choice = tracker.get_slot("contact_name")
 
-#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # if user_choice in contact_list:
+        #     print(f"Now calling the Contacts API to call the person {user_choice}")
+        #     dispatcher.utter_message(f"Now calling the Contacts API to call the person {user_choice}")
+        # else:
+        #     dispatcher.utter_message("The person you are trying to search is not in your contacts")
 
-#         return [SlotSet("API_num", "two")]
+        print(f"Now calling the Contacts API to call the person {user_choice}")
+        dispatcher.utter_message(f"Now calling the Contacts API to call the person {user_choice}")
 
-
-# class ActionCheckLocation(Action):
-
-#     def name(self) -> Text:
-
-#         return "action_check_location"
-
-#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-#         user_choice = tracker.get_slot('location_name')
-#         dispatcher.utter_message(text = f"You chose the location {user_choice}")
-#         dispatcher.utter_message(text = "Was this the location you wanted?")
-
-
-#         return [FollowupAction("action_set_api_slot_maps")]
-
-# class ActionSetAPISlot_Contacts(Action):
-
-#     def name(self) -> Text:
-        
-#         return "action_set_api_slot_contacts"
-
-#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-#         return [SlotSet("API_num", "three")]
-
-# class ActionCheckContacts(Action):
-
-#     def name(self) -> Text:
-
-#         return "action_check_contacts"
-
-#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-#         user_choice = tracker.get_slot('contact_name')
-#         dispatcher.utter_message(text = f"You chose the location {user_choice}")
-#         dispatcher.utter_message(text = "Was this the person you wanted?")
-
-
-#         return [FollowupAction("action_set_api_slot_contact")]
-
-
-
-# class ValidateSimpleMusicForm(FormValidationAction):
-    
-#     def name(self) -> Text:
-#         return "validate_simple_music_form"
-
-#     def validate_song_name(self, slot_value: Any, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-#         user_choice = tracker.get_slot('song_name')
-
-#         if user_choice in range(1000, 10000):
-#             dispatcher.utter_message(text = "You have entered a phone number, please request a song")
-        
-#         dispatcher.utter_message(text = f"You have chosen the song {user_choice}")
-
-#         user_conf = tracker.get_slot('song_confirmation')
-#         if user_conf == 'yes':
-#             return [FollowupAction('action_play_song')]
-
-#         return []
-        
-        # user_conf = tracker.get_slot('song_confirmation')
-
-        # if user_conf == 'yes' | 'Yes' | 'YES':
-
-        # return [FollowupAction("action_play_song")]
-        
-        # if user_conf == 'no':
-            
-        #     dispatcher.utter_message(text = f"Oops! Seems like I heard you wrong! Please repeat the song name!")
-
-        #     return [FollowupAction("validate_simple_music_form")]
-
+        return []
